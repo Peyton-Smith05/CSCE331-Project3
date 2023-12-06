338088dd66ce18e186e7ff81388b32c8892d059d
Michael Morris <MRMorris64@tamu.edu>
2023-12-05 22:54:35 -0600

    "Login page font size"

--- client/src/Login.vue
+++ client/src/Login.vue
@@ -1,174 +1,177 @@
 <template>
-<body>        
-    <div class="login-page">
-        <LoginPopup v-if="popupError" :closePopup="() => closePopup()"> 
-            <h2> Log in error: </h2>
-            <br>
-            An incorrect email/password was provided. Please make sure to type the correct user information.
-            <br>
-        </LoginPopup>   
+    <body>
+      <div class="login-page">
+        <LoginPopup v-if="popupError" :closePopup="closePopup">
+          <h2> Log in error: </h2>
+          <br />
+          An incorrect email/password was provided. Please make sure to type the correct user information.
+          <br />
+        </LoginPopup>
         <div class="login-logo">
-            <img id="kft-banner" src="./assets/KFTBanner.jpg" alt="">
+          <img id="kft-banner" src="./assets/KFTBanner.jpg" alt="" />
         </div>
-        <img id="title" src="./assets/Kung_Fu_Tea_Official_Logo.png" alt="">
+        <TextSlider></TextSlider>
+        <img id="title" src="./assets/Kung_Fu_Tea_Official_Logo.png" alt="" />
         <div class="login-entry">
-            <form class="login-container">
-                <label id="email-label" for="email">Login:</label>
-                <input id="email-entry" type="text" v-model="email" placeholder="email@example.com">
-                <br>
-                <label id="pswd-label" for="pswd">Password:</label>
-                <input id="pswd-entry" type="password" v-model="pswd" placeholder="e.g 1234">
-                <br>
-                <div class="login-sections">
-                    <input @click="login()" id="submit-btn" type="button" value="Submit">
-                    <br>
-                    <GoogleLogin class="google-oauth" :callback="callback" prompt auto-login/>
-                </div>
-            </form>
+          <form class="login-container">
+            <label id="email-label" :style="{ fontSize: LogPassFontMod }" for="email">Login:</label>
+            <input id="email-entry" :style="{ fontSize: LogPassFontMod }" type="text" v-model="email" placeholder="email@example.com" />
+            <br />
+            <label id="pswd-label" :style="{ fontSize: LogPassFontMod }" for="pswd">Password:</label>
+            <input id="pswd-entry" :style="{ fontSize: LogPassFontMod }" type="password" v-model="pswd" placeholder="e.g 1234" />
+            <br />
+            <div class="login-sections">
+              <input @click="login" :style="{ fontSize: LogPassFontMod }" id="submit-btn" type="button" value="Submit" />
+              <br />
+              <GoogleLogin class="google-oauth" :callback="handleGoogleOAuth" prompt auto-login />
+            </div>
+          </form>
         </div>
-    </div>
-</body>
-</template>
-<script>
-import axios from 'axios'
-import { googleOneTap } from 'vue3-google-login'
-import { decodeCredential } from 'vue3-google-login'
-import LoginPopup from './components/LoginPopup.vue'
+      </div>
+    </body>
+  </template>
 
-const apiRedirect = (window.location.href.slice(0,17) == "http://localhost:") ? "http://localhost:3000" : "";
+
+<script>
+import axios from 'axios';
+import { googleOneTap } from 'vue3-google-login';
+import { decodeCredential } from 'vue3-google-login';
+import LoginPopup from './components/LoginPopup.vue';
+import { ref, inject, computed} from 'vue';
+import TextSlider from './TextSlider.vue';
+
+
+const apiRedirect =
+  window.location.href.slice(0, 17) === 'http://localhost:'
+    ? 'http://localhost:3000'
+    : '';
 
 export default {
-    components: {
-        LoginPopup
-    },
-    data() {
-        return {
-            callback:(response) => {
-                googleOneTap({ autoLigin: true })
-                .then((response) => {
-                    const userData = decodeCredential(response.credential)
-                    this.email = userData.email
-                    // Handling email from backend.
-                    handleGoogleOAuth();
-                })
-                .catch((error) => {
-                    console.error(error);
-                })
-            },
-            empid: 0,
-            email: '',
-            pswd:  '',
-            popupError: false,
+  components: {
+    LoginPopup,
+    TextSlider
+},
+  setup() {
+    const globalData = inject('globalTextMod');
+    const empid = ref(0);
+    const email = ref('');
+    const pswd = ref('');
+    const popupError = ref(false);
+
+
+    const login = async () => {
+      try {
+        const login_query = `${apiRedirect}/login/info/${email.value}`;
+        const response = await axios.get(login_query);
+
+        if (response.data.length === 0) {
+          goToCustomer();
         }
-    },
-    methods: {
-        async login() {
-            try {
-                const login_query = apiRedirect + "/login/info/" + this.email;
-                const response = await axios.get(login_query);
-                // If it is no cashier/manager simply go straight to customer.
-                if(response.data.length == 0) {this.goToCustomer();   }
-                const user_info = response.data[0];
-                if(user_info.password !== this.pswd) {throw new Error("Invalid Password was provided");}
-                // Start routing to customer, cashier, and manager.
-                if(user_info.title == "Cashier") {
-                    this.empid = user_info.empid;
-                    this.goToCashier(user_info.empid);
-                } else if(user_info.title == "Manager") {
-                    this.goToManager();
-                }
-            } catch (error) {
-                this.openPopup()
-                console.error(error);
-            }
-        },
-        async handleGoogleOAuth() {
-            try {
-                const google_login_query = apiRedirect + "/login/info/" + this.email;
-                const response = await axios.get(google_login_query);
-                const user_info = response.data[0];
-                
-                if(!user_info) {
-                    this.goToCustomer();
-                }
 
-                if(this.pswd !== user_info.password) {throw new Error("Invalid password")};
+        const user_info = response.data[0];
 
-                // Start routing to customer, cashier, and manager.
-                if(user_info.title == "Cashier") {
-                    this.goToCashier();
-                } else if(user_info.title == "Manager") {
-                    this.goToManager();
-                }
-            } catch (error) {
-                this.openPopup()
-                console.error(error);
-            }
-        },
-        async handleGoogleOAuth() {
-            try {
-                const google_login_query = apiRedirect + "/login/info/" + this.email;
-                const response = await axios.get(google_login_query);
-                const user_info = response.data[0];
-                
-                // Start routing to customer, cashier, and manager.
-                if(user_info.title == "Cashier") {
-                    this.goToCashier();
-                } else if(user_info.title == "Manager") {
-                    this.goToManager();
-                } else {
-                    this.goToCustomer();   
-                }
-            } catch (error) {
-                this.goToCashier();
-            }
-        },
+        if (user_info.password !== pswd.value) {
+          throw new Error('Invalid Password was provided');
+        }
 
-        openPopup() {
-            this.popupError = true;
+        if (user_info.title === 'Cashier') {
+          empid.value = user_info.empid;
+          goToCashier(user_info.empid);
+        } else if (user_info.title === 'Manager') {
+          goToManager();
+        }
+      } catch (error) {
+        openPopup();
+        console.error(error);
+      }
+    };
+
+    const LogPassFontMod = computed(() => {
+        const textSize = 20;
+        return `${textSize * globalData.textMod}px`;
+    });
+
+    const handleGoogleOAuth = async () => {
+      try {
+        const google_login_query = `${apiRedirect}/login/info/${email.value}`;
+        const response = await axios.get(google_login_query);
+        const user_info = response.data[0];
+
+        if (!user_info) {
+          goToCustomer();
+        }
+
+        if (pswd.value !== user_info.password) {
+          throw new Error('Invalid password');
+        }
+
+        if (user_info.title === 'Cashier') {
+          goToCashier();
+        } else if (user_info.title === 'Manager') {
+          goToManager();
+        } else {
+          goToCustomer();
+        }
+      } catch (error) {
+        openPopup();
+        console.error(error);
+      }
+    };
+
+    const openPopup = () => {
+      popupError.value = true;
+    };
+
+    const closePopup = () => {
+      popupError.value = false;
+    };
+
+    const goToCashier = (empid) => {
+      this.$router.push({
+        name: 'Cashier',
+        query: {
+          empid: JSON.stringify(empid),
         },
-        closePopup() {
-            this.popupError = false;
+      });
+    };
+
+    const goToCustomer = () => {
+      this.$router.push({
+        name: 'Customer',
+        query: {
+          empid: JSON.stringify(0),
         },
-        
-        goToCashier() {
-            // Navigate to the cashier interface page using Vue Router
-            this.$router.push({
-                name: 'Cashier',
-                query: {
-                    empid: JSON.stringify(this.empid),
-                }
-            });
+      });
+    };
+
+    const goToManager = (empid) => {
+      console.log('Rerouting to manager side');
+      this.$router.push({
+        name: 'Manager',
+        query: {
+          empid: JSON.stringify(empid),
         },
-        goToCustomer() {
-            this.$router.push({
-                name: 'Customer',
-                query: {
-                    empid: JSON.stringify(0),
-                }
-            });
-        },
-        goToManager(empid) {
-            // TODO create rerouting to manager site.
-            console.log("Rerouting to manager side");
-            this.$router.push({
-                name: 'Manager',
-                query: {
-                    empid: JSON.stringify(empid),
-                }
-            });
-        },
-        goToCustomer() {
-            this.$router.push('/cashier');
-        },
-        goToManager() {
-            // TODO create rerouting to manager site.
-            console.log("Rerouting to manager side");
-            this.$router.push('/manager');
-        },
-    },
-}
+      });
+    };
+
+    return {
+      empid,
+      email,
+      pswd,
+      popupError,
+      login,
+      handleGoogleOAuth,
+      openPopup,
+      closePopup,
+      goToCashier,
+      goToCustomer,
+      goToManager,
+      LogPassFontMod,
+    };
+  },
+};
 </script>
+
 <style>
 .login-page {
     position: absolute;
